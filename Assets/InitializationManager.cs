a60a297b823f45a13c8c68b49d997ccf900e4415
Serkan <yilserkan66@gmail.com>
2024-06-28 18:18:17 +0300

    feat : initialization manager intializes now scriptable managers before switching to game scene

--- Assets/InitializationManager.cs
+++ Assets/InitializationManager.cs
@@ -1,4 +1,6 @@
 using RPGGame.SceneManagement;
+using RPGGame.Utils;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +9,30 @@ namespace RPGGame.Initialization
 {
     public class InitializationManager : MonoBehaviour
     {
+
+        [SerializeField] private ScriptableObjectBase[] _scriptableObjectBases;
+
         void Start()
         {
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            for (int i = 0; i < _scriptableObjectBases.Length; i++)
+            {
+                await _scriptableObjectBases[i].Initialize();
+            }
+
             SceneLoader.Instance.LoadScene(SceneType.GameScene);
+        }
+
+        private void OnApplicationQuit()
+        {
+            for (int i = 0; i < _scriptableObjectBases.Length; i++)
+            {
+                _scriptableObjectBases[i].Destroy();
+            }
         }
     }
 }
