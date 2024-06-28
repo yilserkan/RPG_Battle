6c7418c7edc789b8dd6d77fdf074d06e963c5bea
Serkan <yilserkan66@gmail.com>
2024-06-28 15:50:25 +0300

    feat : created don destroy on load script

---
+++ Assets/Scripts/Utils/DontDestroyGameObjectOnLoad.cs
@@ -0,0 +1,12 @@
+using RPGGame.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame.Utils
+{
+    public class DontDestroyGameObjectOnLoad : AbstractPersistentMonoSingleton<DontDestroyGameObjectOnLoad>
+    {
+
+    }
+}
