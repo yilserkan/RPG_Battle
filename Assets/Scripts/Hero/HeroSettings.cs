4933d5b536996346f0a7d8bad1bd18f1b1b987b1
Serkan <yilserkan66@gmail.com>
2024-06-29 23:41:47 +0300

    Asset : added hero sprites to hero settings

--- Assets/Scripts/Hero/HeroSettings.cs
+++ Assets/Scripts/Hero/HeroSettings.cs
@@ -11,9 +11,11 @@ namespace RPGGame.Hero
         [SerializeField] private string _id;
         [SerializeField] private string _name;
         [SerializeField] private CharacterBaseStats _baseStats;
+        [SerializeField] private Sprite _heroSprite;
 
         public string ID => _id;
         public string Name => _name;
         public CharacterBaseStats BaseStats => _baseStats;
+        public Sprite HeroSprite => _heroSprite;
     }
 }
