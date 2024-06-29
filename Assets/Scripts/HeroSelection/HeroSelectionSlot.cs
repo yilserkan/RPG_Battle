e8b105aec8b78b1d90d57d1a54d9fa8a656c6726
Serkan <yilserkan66@gmail.com>
2024-06-29 23:45:38 +0300

    feat : hero slot requests hero info popup when holded for 3 seconds

--- Assets/Scripts/HeroSelection/HeroSelectionSlot.cs
+++ Assets/Scripts/HeroSelection/HeroSelectionSlot.cs
@@ -1,3 +1,5 @@
+using RPGGame.Utils;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,11 +10,27 @@ namespace RPGGame.HeroSelection
 {
     public class HeroSelectionSlot : MonoBehaviour
     {
+        [SerializeField] private HeroSelectionSlotUI _slotUI;
         [SerializeField] private Image _heroIcon;
         [SerializeField] private TextMeshProUGUI _heroNameText;
+        [SerializeField] private CustomHoldableButton _customHoldableButton;
+
+        private Hero.Hero _hero;
+        public static event Action<Hero.Hero> RequestHeroPopupEvent;
+
+        private void OnEnable()
+        {
+            AddListeners();   
+        }
+
+        private void OnDisable()
+        {
+            RemoveListeners();
+        }
 
         public void SetupSlot(Hero.Hero hero)
         {
+            _hero = hero;
             var isSlotOccupied = hero != null;
 
             if (isSlotOccupied)
@@ -28,12 +46,28 @@ namespace RPGGame.HeroSelection
         private void SetupOccupiedSlot(Hero.Hero hero)
         {
             _heroIcon.gameObject.SetActive(true);
-            _heroNameText.text = hero._settings.Name;
+            _heroIcon.sprite = hero.Settings.HeroSprite;
+            _heroNameText.text = hero.Settings.Name;
         }
 
         private void SetupEmptySlot()
         {
             _heroIcon.gameObject.SetActive(false);
+        }
+
+        private void RequestHeroPopup()
+        {
+            RequestHeroPopupEvent?.Invoke(_hero);
+        }
+
+        private void AddListeners()
+        {
+            _customHoldableButton.OnHold += RequestHeroPopup;
+        }
+
+        private void RemoveListeners()
+        {
+            _customHoldableButton.OnHold -= RequestHeroPopup;
         }
     }
 }
