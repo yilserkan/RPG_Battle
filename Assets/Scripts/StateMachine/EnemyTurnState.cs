c78cc73d21898756707e3948cb8cb31507069a7c
Serkan <yilserkan66@gmail.com>
2024-07-01 18:10:51 +0300

    feat : created player turn and enemy turn state

---
+++ Assets/Scripts/StateMachine/EnemyTurnState.cs
@@ -0,0 +1,25 @@
+﻿using RPGGame.Game;
+
+namespace RPGGame.StateMachine
+{
+    public class EnemyTurnState : BaseState
+    {
+        public EnemyTurnState(GameStateMachine stateMachine) : base(stateMachine)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            AttackRandomPlayer();
+            _stateMachine.SwitchState(GameStates.PlayerTurn);
+        }
+
+        private void AttackRandomPlayer()
+        {
+            var attacker = _stateMachine.GetRandomGameHero(HeroTeam.Enemy);
+            var receiver = _stateMachine.GetRandomGameHero(HeroTeam.Player);
+
+            attacker.Attack(receiver);
+        }
+    }
+}
