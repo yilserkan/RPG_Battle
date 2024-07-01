6149945507b5df3763ff79d0815d5b0b267d0489
Serkan <yilserkan66@gmail.com>
2024-07-01 14:36:50 +0300

    feat : created initialization state for state machine

---
+++ Assets/Scripts/StateMachine/InitializationState.cs
@@ -0,0 +1,38 @@
+﻿using RPGGame.Game;
+
+namespace RPGGame.StateMachine
+{
+    public class InitializationState : BaseState
+    {
+        public InitializationState(GameStateMachine stateMachine) : base(stateMachine)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            InitializeGameState();
+        }
+
+        private void InitializeGameState()
+        {
+            var levelData = _stateMachine.LevelData;
+
+            _stateMachine.HeroSpawner.CreateHeroes(levelData.PlayerHeroes);
+            _stateMachine.HeroSpawner.CreateHeroes(levelData.EnemyHeroes);
+
+            var nextState = GetNextState(levelData);
+            _stateMachine.SwitchState(nextState);
+        }
+
+        private GameStates GetNextState(LevelData levelData)
+        {
+            if(levelData.CurrentState == (int)GameStates.None)
+            {
+                return GameStates.None;
+            }
+
+            return (GameStates)levelData.CurrentState;
+        }
+    }
+}
