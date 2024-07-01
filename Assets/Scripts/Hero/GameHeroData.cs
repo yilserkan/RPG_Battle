fed914536584ffc7c066cdd8906f53d8769b7557
Serkan <yilserkan66@gmail.com>
2024-07-01 14:35:57 +0300

    feat : created game hero data

---
+++ Assets/Scripts/Hero/GameHeroData.cs
@@ -0,0 +1,27 @@
+﻿using RPGGame.Game;
+using RPGGame.Stats;
+using System;
+
+namespace RPGGame.Hero
+{
+    [Serializable]
+    public class GameHeroData : HeroData
+    {
+        public float RemainingVitality;
+
+        public GameHeroData(string id, HeroTeam team) : base(id, team)
+        {
+        }
+
+        public GameHeroData(Hero hero) : base(hero)
+        {
+            RemainingVitality = hero.Stats.CalculateAttributeValue(StatConstants.Vitality);
+        }
+
+        public GameHeroData(GameHero gameHero) : base(gameHero.Hero)
+        {
+            RemainingVitality = gameHero.Vitality;
+        }
+    }
+}
+
