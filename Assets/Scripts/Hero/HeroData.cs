3c53c6a4ff52d75f1519f12660a73a2b6e37ef8e
Serkan <yilserkan66@gmail.com>
2024-07-01 14:33:43 +0300

    refactor : added hero team to hero data,  moved hero data and hero data wrapper to its own files

---
+++ Assets/Scripts/Hero/HeroData.cs
@@ -0,0 +1,31 @@
+﻿using RPGGame.Game;
+using System;
+
+namespace RPGGame.Hero
+{
+    [Serializable]
+    public class HeroData
+    {
+        public string ID;
+        public int Level;
+        public int Experience;
+        public int HeroTeam;
+
+        public HeroData(string id, HeroTeam team)
+        {
+            ID = id;
+            Level = 1;
+            Experience = 0;
+            HeroTeam = (int)team;
+        }
+
+        public HeroData(Hero hero)
+        {
+            ID = hero.Settings.ID;
+            Level = hero.Level;
+            Experience = hero.Experience;
+            HeroTeam = hero.HeroTeam;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Hero/HeroDataWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPGGame.Hero
+{
+    [Serializable]
+    public class HeroDataWrapper
+    {
+        public HeroData[] HeroDatas;
+
+        public HeroDataWrapper(List<Hero> heroes)
+        {
+            HeroDatas = new HeroData[heroes.Count];
+            for (int i = 0; i < heroes.Count; i++)
+            {
+                HeroDatas[i] = new HeroData(heroes[i]);
+            }
+        }
+    }
+}
+
