7cd61cc94a641d97c61040a58c20160c5e83442c
Serkan <yilserkan66@gmail.com>
2024-07-01 20:42:54 +0300

    Asset : added skill to hero settings

--- Assets/Scripts/Hero/HeroSettings.cs
+++ Assets/Scripts/Hero/HeroSettings.cs
@@ -1,3 +1,4 @@
+using RPGGame.Skills;
 using RPGGame.Stats;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,11 +14,13 @@ namespace RPGGame.Hero
         [SerializeField] private CharacterBaseStats _baseStats;
         [SerializeField] private Sprite _heroSprite;
         [SerializeField] private AnimatorOverrideController _animatorOverrideController;
+        [SerializeField] private AbstractSkillSettings _skill;
 
         public string ID => _id;
         public string Name => _name;
         public CharacterBaseStats BaseStats => _baseStats;
         public Sprite HeroSprite => _heroSprite;
         public AnimatorOverrideController AnimatorOverrideController => _animatorOverrideController;
+        public AbstractSkillSettings Skill => _skill;
     }
 }
