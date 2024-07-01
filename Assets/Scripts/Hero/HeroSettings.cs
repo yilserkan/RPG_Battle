6c2114e39ac58970f1e8bb781ee702c38a1e0aed
Serkan <yilserkan66@gmail.com>
2024-07-01 16:00:10 +0300

    Asset : created idle, death, take damage and attack animations for all hereos

--- Assets/Scripts/Hero/HeroSettings.cs
+++ Assets/Scripts/Hero/HeroSettings.cs
@@ -12,10 +12,12 @@ namespace RPGGame.Hero
         [SerializeField] private string _name;
         [SerializeField] private CharacterBaseStats _baseStats;
         [SerializeField] private Sprite _heroSprite;
+        [SerializeField] private AnimatorOverrideController _animatorOverrideController;
 
         public string ID => _id;
         public string Name => _name;
         public CharacterBaseStats BaseStats => _baseStats;
         public Sprite HeroSprite => _heroSprite;
+        public AnimatorOverrideController AnimatorOverrideController => _animatorOverrideController;
     }
 }
