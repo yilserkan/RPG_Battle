24719f409ac42e13f417cb90187538900c2b28de
Serkan <yilserkan66@gmail.com>
2024-07-01 20:40:15 +0300

    fix : enemy turn state and player tunr state wait now for the attack animations to complete

--- Assets/Scripts/StateMachine/EnemyTurnState.cs
+++ Assets/Scripts/StateMachine/EnemyTurnState.cs
@@ -1,25 +1,49 @@
 ﻿using RPGGame.Game;
+using System;
 
 namespace RPGGame.StateMachine
 {
     public class EnemyTurnState : BaseState
     {
+        private const float _switchStateDelay = .5f;
+
         public EnemyTurnState(GameStateMachine stateMachine) : base(stateMachine)
         {
         }
 
         public override void OnEnter()
         {
+            base.OnEnter();
+            AddListeners();
             AttackRandomPlayer();
-            _stateMachine.SwitchState(GameStates.PlayerTurn);
         }
 
+        public override void OnExit()
+        {
+            base.OnExit();
+            RemoveListeners();
+        }
         private void AttackRandomPlayer()
         {
             var attacker = _stateMachine.GetRandomGameHero(HeroTeam.Enemy);
             var receiver = _stateMachine.GetRandomGameHero(HeroTeam.Player);
 
             attacker.Attack(receiver);
+        }
+
+        private void OnHeroCompletedAttack()
+        {
+            _stateMachine.DelayedSwitchState(GameStates.PlayerTurn, _switchStateDelay);
+        }
+
+        private void AddListeners()
+        {
+            GameHero.OnHeroCompletedAttack += OnHeroCompletedAttack;
+        }
+
+        private void RemoveListeners()
+        {
+            GameHero.OnHeroCompletedAttack -= OnHeroCompletedAttack;
         }
     }
 }
--- Assets/Scripts/StateMachine/PlayerTurnState.cs
+++ Assets/Scripts/StateMachine/PlayerTurnState.cs
@@ -8,37 +8,49 @@ namespace RPGGame.StateMachine
         public static event Action OnEnablePlayerSelectionEvent;
         public static event Action OnDisablePlayerSelectionEvent;
 
+        private bool _hasPlayerSelectedHero;
+        private const float _switchStateDelay = .5f;
+
         public PlayerTurnState(GameStateMachine stateMachine) : base(stateMachine)
         {
         }
 
         public override void OnEnter()
         {
+            base.OnEnter();
             AddListeners();
+            _hasPlayerSelectedHero = false;
             OnEnablePlayerSelectionEvent?.Invoke();
         }
         public override void OnExit()
         {
-            RemoveListeners();
             base.OnExit();
+            RemoveListeners();
         }
         private void HandleOnHeroSelected(GameHero hero)
         {
+            if (_hasPlayerSelectedHero) { return; }
+            _hasPlayerSelectedHero = true;
+
             var target = _stateMachine.GetRandomGameHero(HeroTeam.Enemy);
             hero.Attack(target);
-            _stateMachine.SwitchState(GameStates.EnemyTurn);
         }
 
-     
+        private void OnHeroCompletedAttack()
+        {
+            _stateMachine.DelayedSwitchState(GameStates.EnemyTurn, _switchStateDelay);
+        }
 
         private void AddListeners()
         {
             GameHeroSelectionController.OnHeroSelectedToAttack += HandleOnHeroSelected;
+            GameHero.OnHeroCompletedAttack += OnHeroCompletedAttack;
         }
 
         private void RemoveListeners()
         {
             GameHeroSelectionController.OnHeroSelectedToAttack -= HandleOnHeroSelected;
+            GameHero.OnHeroCompletedAttack -= OnHeroCompletedAttack;
         }
     }
 }
