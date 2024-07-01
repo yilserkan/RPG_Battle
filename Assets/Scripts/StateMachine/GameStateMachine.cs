eaf9ee982264adaf335712f9d2288e2990a8007f
Serkan <yilserkan66@gmail.com>
2024-07-01 18:12:34 +0300

    fix : added playerTurn and enemyTurn gameStates to GameStateMachine

--- Assets/Scripts/StateMachine/GameStateMachine.cs
+++ Assets/Scripts/StateMachine/GameStateMachine.cs
@@ -1,18 +1,46 @@
+using RPGGame.Game;
+using RPGGame.Level;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace RPGGame.StateMachine
 {
     public class GameStateMachine : MonoBehaviour
     {
+        [SerializeField] private GameHeroSpawner _heroSpawner;
+
+        public GameHeroSpawner HeroSpawner => _heroSpawner;
+
         private BaseState _currentState;
 
-        private Dictionary<GameStates, BaseState> _states =
+        private Dictionary<GameStates, BaseState> _states;
+
+        private LevelData _levelData;
+        public LevelData LevelData => _levelData;
+        private Dictionary<HeroTeam, GameHero[]> _gameHeroesDict;
+        public Dictionary<HeroTeam, GameHero[]> GameHeroesDic => _gameHeroesDict;
+
+        private void Start()
+        {
+            _states =
             new Dictionary<GameStates, BaseState>()
             {
-                //{ GameStates.None, new BaseState() }
+                { GameStates.Initial, new InitializationState(this) },
+                { GameStates.PlayerTurn, new PlayerTurnState(this) },
+                { GameStates.EnemyTurn, new EnemyTurnState(this) }
             };
+
+            _gameHeroesDict = new Dictionary<HeroTeam, GameHero[]>();
+        }
+
+        public void StartLevel(LevelData levelData)
+        {
+            _levelData = levelData;
+            SwitchState(GameStates.Initial);
+        }
 
         public void SwitchState(GameStates newState)
         {
@@ -36,12 +64,27 @@ namespace RPGGame.StateMachine
             _currentState?.OnUpdate();
         }
 
+        public void SetGameHeroesDict(Dictionary<HeroTeam, GameHero[]> heroes)
+        {
+            _gameHeroesDict = heroes;
+        }
+
+        public GameHero GetRandomGameHero(HeroTeam team)
+        {
+            var enemyHeroes = GameHeroesDic[team];
+            var heroes = enemyHeroes.Where(hero => hero.Vitality > 0).ToArray();
+            int randIndex = Random.Range(0, heroes.Length);
+            return heroes[randIndex];
+        }
+
     }
 
     public enum GameStates
     {
         None,
-
+        Initial,
+        PlayerTurn,
+        EnemyTurn
     }
 }
 
