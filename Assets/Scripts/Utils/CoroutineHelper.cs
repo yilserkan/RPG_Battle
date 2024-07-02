89f88d0ac932fb01557560a3d38297a748f503d8
Serkan <yilserkan66@gmail.com>
2024-07-02 14:32:55 +0300

    feat : created coroutine helper class which saves created WaitForSeconds and returns them instead of createing a new one everytine to save performance

---
+++ Assets/Scripts/Utils/CoroutineHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame.Utils
+{
+    public static class CoroutineHelper
+    {
+        private static Dictionary<float, WaitForSeconds> _waitForSecondsDict = new Dictionary<float, WaitForSeconds>();
+
+        public static WaitForSeconds GetWaitForSeconds(float duration)
+        {
+            if (_waitForSecondsDict.ContainsKey(duration))
+                return _waitForSecondsDict[duration];
+
+            var waitForSeconds = new WaitForSeconds(duration);
+            _waitForSecondsDict.Add(duration, waitForSeconds);
+            return waitForSeconds;
+        }
+    }
+}
