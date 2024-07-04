190131421fd96e7d8f965439d32e618a3bdf1836
Serkan <yilserkan66@gmail.com>
2024-07-04 11:34:33 +0300

    fix : player hero settings in levelManager and GameHeroSpawner are being get from PlayerData

--- Assets/Scripts/Game/GameHeroSpawner.cs
+++ Assets/Scripts/Game/GameHeroSpawner.cs
@@ -4,20 +4,19 @@ using RPGGame.Hero;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPGGame.Player;
 
 namespace RPGGame.Level
 {
     public class GameHeroSpawner : MonoBehaviour
     {
-        [SerializeField] private HeroSettingsContainer _heroSettings;
         [SerializeField] private ObjectPoolSettings _heroFlyweightSettings;
-
         [SerializeField] private Transform _heroParentTransform;
         [SerializeField] private SpawnPointManager _spawnPointManager;
 
         private IGameHeroFactory _heroFactory;
 
-        private void Start()
+        private void Awake()
         {
             _heroFactory = new GameHeroFactory(_heroFlyweightSettings, _heroParentTransform);
         }
@@ -28,7 +27,7 @@ namespace RPGGame.Level
             var heroes = new GameHero[gameHeroDatas.Length];
             for (int i = 0; i < gameHeroDatas.Length; i++)
             {
-                var gameHero = _heroFactory.Create(_heroSettings, gameHeroDatas[i]);
+                var gameHero = _heroFactory.Create(PlayerData.HeroSettingsContainer, gameHeroDatas[i]);
                 heroes[i] = gameHero;
                 var spawnPoint = _spawnPointManager.GetSpawnPoint(heroTeam);
                 heroes[i].SetSpawnPoint(spawnPoint);
